4dd5baf0cf8e0f5e61ea35e179b27014e24d1590
Tarun <tarun1995gupta@gmail.com>
2021-02-01 14:15:48 +0000

    conf

--- maven_code/src/modules/agents/__init__.py
+++ maven_code/src/modules/agents/__init__.py
@@ -3,7 +3,9 @@ REGISTRY = {}
 from .rnn_agent import RNNAgent
 from .noise_rnn_agent import RNNAgent as NoiseRNNAgent
 from .ff_agent import FFAgent
+from .noise_rnn_agent_deep import RNNAgentDeep as NoiseRNNAgentDeep
 
 REGISTRY["rnn"] = RNNAgent
 REGISTRY["ff"] = FFAgent
 REGISTRY["noise_rnn"] = NoiseRNNAgent
+REGISTRY["noise_rnn_deep"] = NoiseRNNAgentDeep
